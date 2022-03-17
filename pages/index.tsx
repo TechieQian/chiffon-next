c2de15634ec6aeac8a6f05087e880091bb74a92f
qwan <qiansan@gmail.com>
2022-03-17 17:41:59 -0400

    editing homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,22 @@ const Home: NextPage = () => {
       June, 2018 and am living the dream in my forever home in New York City. I
       love to dance for treats and accompany my mommy to the dance studio to
       watch her dance too. I also love cooking with my mommy at home! I have fun
-      playing dress up at home and dreaming of being a princess.
+      playing dress up at home and dreaming of being a princess. Check out some
+      of my favorite This or That videos here:
+      <link
+        rel="stylesheet"
+        href="https://www.instagram.com/chiffonthemaltipoo/guide/this-or-that-fashion-and-favorites/17883515288311498/"
+      />
+      Check out some of my favorite Get Ready With Me videos here:
+      <link
+        rel="stylesheet"
+        href="https://www.instagram.com/chiffonthemaltipoo/guide/get-ready-with-me/17954515507677709/"
+      />
+      Check out my organization videos here:
+      <link
+        rel="stylesheet"
+        href="https://www.instagram.com/chiffonthemaltipoo/guide/how-i-organize-my-closet/17972314555482986/"
+      />
     </Body>
   );
 };
